8123031214e6db645d2c2cdbe4db82bfa69fe7bb
mvinyard <mvinyard@broadinstitute.org>
2021-04-12 04:55:41 +0000

    added error metrics might move later

---
+++ scdiffeq/machine_learning/error_metrics.py
@@ -0,0 +1,45 @@
+import torch
+import numpy as np
+
+def calc_perc_error(adata, subset):
+
+    """Calculate the percent error for a set of whole trajectories."""
+    
+    if subset == "test":
+        
+        try:
+            pred_y = adata.uns["latest_test_predictions"].detach().numpy()
+        except:
+            pred_y = adata.uns["latest_test_predictions"]
+        true_y = adata.uns["latest_test_true_y"].detach().numpy()
+        
+        try:
+            pred_y = pred_y.reshape(true_y.shape[0], true_y.shape[1], true_y.shape[2])
+        except:
+            pass
+            
+        
+    elif subset == "training":
+        
+        try:
+            pred_y = adata.uns["latest_training_predictions"].detach().numpy()
+        except:
+            pred_y = adata.uns["latest_training_predictions"]
+        try:
+            true_y = adata.uns["latest_training_true_y"].detach().numpy()
+        except:
+            true_y = adata.uns["latest_training_true_y"]
+            
+    elif subset == "validation":  
+        try:
+            pred_y = adata.uns["latest_validation_predictions"].detach().numpy()
+        except:
+            pred_y = adata.uns["latest_validation_predictions"]
+        try:
+            true_y = adata.uns["latest_validation_true_y"].detach().numpy()
+        except:
+            true_y = adata.uns["latest_validation_true_y"]
+        
+    percent_error = np.sum(np.abs(pred_y - true_y)) / np.sum(true_y) * 100
+            
+    return percent_error
