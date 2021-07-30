307073378a9430aabf6a7c2baa0193f1e917e97d
mvinyard <mvinyard@broadinstitute.org>
2021-07-30 19:04:53 +0000

    updated setup.py with geomloss

--- scdiffeq/utilities/_read_data.py
+++ scdiffeq/utilities/_read_data.py
@@ -1,8 +1,15 @@
-def _read_h5ad(path):
+def _read_h5ad(path, X_to_array=False):
 
     from anndata import read_h5ad
 
     adata = read_h5ad(path)
+    
+    if X_to_array:
+        adata.X = adata.X.toarray()
+    
+    if type(adata.var.index[0]) == str:
+        adata.var = adata.var.reset_index()    
+        adata.var = adata.var.rename({"index": "gene_id"}, axis=1)
 
     print(adata)
     
