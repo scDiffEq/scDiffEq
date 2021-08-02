ad69baaafb027541dd284265703c439ada648cde
mvinyard <mvinyard@broadinstitute.org>
2021-08-02 05:23:36 +0000

    made the add noise func better

--- scdiffeq/utilities/_add_noise.py
+++ scdiffeq/utilities/_add_noise.py
@@ -1,25 +1,44 @@
 
 import numpy as np
 
-def _add_noise(adata, noise=0.05):
+def _add_noise(adata, noise=0.05, uniform=False, return_adata=False):
 
     """
-    Adds random noise to adata.X
+    Adds random noise to adata.X. Loops through each gene and adds noise relative 
+    to the mean expression value of that gene. This function should be run before 
+    preprocessing such that values with noise are scaled.
     
-    Paramters:
+    Parameters:
     ----------
     adata
+        AnnData object. 
     
     
     noise
         default: 0.05
     
-    
+    uniform
+        assumes all genes are the same scale. proceeds more quickly. 
+        
     Returns:
     --------
     
     None
         adata is modified in place.
     """
+    
+    if uniform:
+        adata.X = adata.X + np.random.normal(0, noise, adata.X.shape)
+        
+    else:
+        mean_gex_vals = adata.X.mean(axis=0)
+        noisy_data_X = np.zeros(adata.X.shape)
 
-    adata.X = adata.X + np.random.normal(0, noise, adata.X.shape)
+        for gene, gene_mean in enumerate(mean_gex_vals):
+            single_gene_data = adata.X[:, gene]
+            noisy_data_X[:, gene] = single_gene_data*noise + np.random.normal(
+                0, gene_mean, single_gene_data.shape
+            )
+    
+    if return_adata:
+        return adata
