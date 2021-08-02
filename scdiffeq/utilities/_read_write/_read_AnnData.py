1d4275f38755b775ad52e3c4f782c9e36739271b
mvinyard <mvinyard@broadinstitute.org>
2021-08-02 14:24:48 +0000

    make readadata better

--- scdiffeq/utilities/_read_write/_read_AnnData.py
+++ scdiffeq/utilities/_read_write/_read_AnnData.py
@@ -2,7 +2,28 @@ from glob import glob
 import anndata as a
 import os, pickle
 
-def _read_AnnData(label="testing_results", outpath="./", scdiffeq_outs_dir="scdiffeq_adata"):
+
+def _choose_path_from_glob_list(glob_path_list):
+
+    """"""
+
+    if len(glob_path_list) > 1:
+        print(
+            glob_path_list,
+            "; please narrow your search criteria using the `label` parameter.",
+        )
+    else:
+        glob_path_item = glob_path_list[0]
+
+    return glob_path_item
+
+
+def _read_AnnData(
+    label="testing_results",
+    outpath="./",
+    scdiffeq_outs_dir="scdiffeq_adata",
+    silence=False,
+):
 
     """
     Writes h5ad and pkl file for outputs of scdiffeq method.
@@ -28,10 +49,17 @@ def _read_AnnData(label="testing_results", outpath="./", scdiffeq_outs_dir="scdi
     None
     """
 
-    h5ad_search_path = os.path.join(outpath, scdiffeq_outs_dir, (label + "*")) + ".h5ad"
-    pkl_search_path = os.path.join(outpath, scdiffeq_outs_dir, (label + "*")) + ".pkl"
+    h5ad_search_path = _choose_path_from_glob_list(
+        glob(os.path.join(outpath, scdiffeq_outs_dir, (label + "*h5ad")))
+    )
+    pkl_search_path = _choose_path_from_glob_list(
+        glob(os.path.join(outpath, scdiffeq_outs_dir, (label + "*.pkl")))
+    )
 
     adata = a.read_h5ad(h5ad_search_path)
     adata.uns["pca"] = pickle.load(open(pkl_search_path, "rb"))
+
+    if not silence:
+        print(adata)
 
     return adata
