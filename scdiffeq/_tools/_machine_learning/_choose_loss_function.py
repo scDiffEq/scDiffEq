d12385e946697a2adaa89366b6b4196546b9e3f6
mvinyard <mvinyard@broadinstitute.org>
2021-08-12 06:44:13 +0000

    adding functions to choose torch funcs to learn class

---
+++ scdiffeq/_tools/_machine_learning/_choose_loss_function.py
@@ -0,0 +1,27 @@
+
+# package imports #
+# --------------- #
+import vintools as v
+
+def _choose_loss_function(loss_function):
+
+    """
+    Parameters:
+    -----------
+    loss_function
+        type: str
+
+    Returns:
+    --------
+    imported_torch_loss_func
+        type: torch.nn.loss.<LossFunc_module>
+
+    Notes:
+    ------
+    """
+
+    imported_torch_loss_func = v.ut.import_from_string(
+        package="torch", module="nn", function=loss_function
+    )()
+
+    return imported_torch_loss_func
---
+++ scdiffeq/_tools/_machine_learning/_choose_optimizer.py
@@ -0,0 +1,23 @@
+
+# package imports #
+# --------------- #
+import vintools as v
+
+
+def _choose_optimizer(self, optimizer_choice, learning_rate):
+
+    """
+    optimizer_choice
+    
+    learning_rate
+    
+    """
+
+    optimizer_func = v.ut.import_from_string(
+        "torch",
+        "optim",
+        optimizer_choice,
+    )
+    optimizer = optimizer_func(self.network.parameters(), lr=learning_rate)
+    
+    return optimizer
