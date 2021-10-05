6138f66f1a7641afc5bfc1f532d9c675f792c625
mvinyard <mvinyard@broadinstitute.org>
2021-10-05 18:27:24 +0000

    add LARRY subset NM torch data

--- scdiffeq/_data/__init__.py
+++ scdiffeq/_data/__init__.py
@@ -4,3 +4,4 @@ __author__ = ", ".join(["Michael E. Vinyard"])
 __email__ = ", ".join(["vinyard@g.harvard.edu",])
 
 from ._simulated_data._GenericSimulator import _GenericSimulator as GenericSimulator
+from ._data._load_LARRY_NeutrophilMonocyte_subset import _load_LARRY_NeutrophilMonocyte_subset as LARRY_NM_subset
