b7bf4df2171a0a605cc7649c9e8bba6f12325815
mvinyard <mvinyard@broadinstitute.org>
2021-10-05 18:31:43 +0000

    fix small nomenclature bugs

--- scdiffeq/_data/__init__.py
+++ scdiffeq/_data/__init__.py
@@ -4,4 +4,4 @@ __author__ = ", ".join(["Michael E. Vinyard"])
 __email__ = ", ".join(["vinyard@g.harvard.edu",])
 
 from ._simulated_data._GenericSimulator import _GenericSimulator as GenericSimulator
-from ._data._load_LARRY_NeutrophilMonocyte_subset import _load_LARRY_NeutrophilMonocyte_subset as LARRY_NM_subset
+from ._real_data._load_LARRY_NeutrophilMonocyte_subset import _load_LARRY_NeutrophilMonocyte_subset as LARRY_NM_subset
