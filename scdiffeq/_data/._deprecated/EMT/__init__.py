7c1038c0f3785bce456028be64fcdd1643351408
mvinyard <mvinyard@broadinstitute.org>
2022-03-15 19:26:03 +0000

    removing old data

--- scdiffeq/_data/._deprecated/EMT/__init__.py
+++
@@ -1,9 +0,0 @@
-# EMT simulation __init__.py
-
-__author__ = ", ".join(["Michael E. Vinyard"])
-__email__ = ", ".join(["vinyard@g.harvard.edu",])
-
-
-from .run_emt import simulate_iteratively as simulate
-from .plot_EMT import plot_EMH
-from .utils import grab_stable_state_conditions as get_ss
