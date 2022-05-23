f3fe8f80cb6344608155ade77e428bed2a59703e
mvinyard <mvinyard@broadinstitute.org>
2022-05-23 15:08:45 +0000

    setting up io structure

---
+++ scdiffeq/_io/__init__.py
@@ -0,0 +1,7 @@
+
+__module_name__ = "__init__.py"
+__author__ = ", ".join(["Michael E. Vinyard"])
+__email__ = ", ".join(["vinyard@g.harvard.edu",])
+
+
+from ._read_h5ad import _read_h5ad as read_h5ad
