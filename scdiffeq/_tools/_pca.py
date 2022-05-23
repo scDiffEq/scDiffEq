fde565c29bc7db04500473ae359a2eaf5e839995
mvinyard <mvinyard@broadinstitute.org>
2022-05-23 16:58:01 +0000

    Add pca

--- scdiffeq/_tools/_pca.py
+++ scdiffeq/_tools/_pca.py
@@ -3,3 +3,123 @@ __module_name__ = "_pca.py"
 __author__ = ", ".join(["Michael E. Vinyard"])
 __email__ = ", ".join(["vinyard@g.harvard.edu",])
 
+
+# import packages #
+# --------------- #
+from sklearn.decomposition import PCA
+from sklearn import preprocessing
+
+
+def _preprocess_for_pca(X, **kwargs):
+
+    """
+    Scale data for input to PCA by standarizing features using the
+    sklearn.preprcessing.StandardScaler module. This function removes the
+    mean of the data across the samples axis and scales to the unit
+    variance of the sample. This is a typical preprocessing step prior to PCA.
+
+    Parameters:
+    -----------
+    X
+        type: numpy.ndarray
+
+    kwargs:
+    -------
+    copy
+        defalt: True
+        type: bool
+
+    with_mean
+        defalt: True
+        type: bool
+
+    with_std
+        defalt: True
+        type: bool
+
+    Returns:
+    --------
+    X_scaled
+        type: numpy.ndarray
+
+    Notes:
+    ------
+    (1) This function is a wrapper of sklearn.preprcessing.StandardScaler. Further
+        documentation can be found here:
+        https://scikit-learn.org/stable/modules/generated/sklearn.preprocessing.StandardScaler.html
+    """
+
+    scaler = preprocessing.StandardScaler()
+    return scaler.fit_transform(X)
+
+
+def _ensure_numpy_array(X):
+    """
+    checks the class name of the passed array and converts to numpy if not numpy.
+    this may actually be a pretty brittle function as the `.toarray()` method may
+    not work on all array types beyond scipy.sparse. To be tested.
+    """
+    if X.__class__.__name__ == "ndarray":
+        return X
+    else:
+        return X.toarray()
+
+
+def _pca(adata, n_components=50, preprocess=True):
+
+    """
+    PCA-transform adata.X
+
+    Parameters:
+    -----------
+    adata
+        type: anndata._core.anndata.AnnData
+
+    n_components
+        Number of principle components in dimensionally-reduced output.
+        default: 50
+        type: int
+
+    preprocess
+        Indicates if the adata.X matrix should be preprocessed using sklearn.preprocessing.StandardScaler
+        default: True
+        type: bool
+
+    Returns:
+    --------
+    pca
+        PCA model object
+        type: sklearn.decomposition._pca.PCA
+
+
+    Notes:
+    ------
+    (1) adata is modified with the pca-transformed matrix as:
+        adata.obsm["X_pca"] with shape: [M, n_pcs].
+
+    (2) Documentation for sklearn.decomposition.PCA can be found here:
+        https://scikit-learn.org/stable/modules/generated/sklearn.decomposition.PCA.html
+    """
+
+    # ------------------------------------- #
+    # preprocess
+    # ------------------------------------- #
+
+    X_ = _ensure_numpy_array(adata.X)
+
+    if preprocess:
+        X_scaled = _preprocess_for_pca(X_)
+        adata.layers["scaled"] = X_scaled
+    else:
+        X_scaled = X_
+
+    # ------------------------------------- #
+    # PCA transform
+    # ------------------------------------- #
+
+    pca = PCA(n_components=n_components)
+    X_pca = pca.fit_transform(X_scaled)
+
+    adata.obsm["X_pca"] = X_pca
+
+    return pca
