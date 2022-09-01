b33921062e2b070c81f2e5691a4aae9604f9a739
mvinyard <mvinyard@broadinstitute.org>
2022-09-01 03:50:34 +0000

    formatting w/ black

--- scdiffeq/_models/_core/_BaseModel.py
+++ scdiffeq/_models/_core/_BaseModel.py
@@ -4,7 +4,6 @@ __author__ = ", ".join(["Michael E. Vinyard"])
 __email__ = ", ".join(["vinyard@g.harvard.edu",])
 
 
-
 # import packages #
 # --------------- #
 import pytorch_lightning
@@ -20,22 +19,23 @@ class BaseModel(pytorch_lightning.LightningModule):
     def __init__(self):
         super(BaseModel, self).__init__()
 
-    def model_setup(self,
-                    dataset,
-                    func,
-                    log_path="./",
-                    learning_rates=[1e-3],
-                    optimizers=[torch.optim.Adam],
-                    schedulers=[torch.optim.lr_scheduler.StepLR],
-                    scheduler_kwargs=[{"step_size":100, "gamma":0.9}],
-                    logger=pytorch_lightning.loggers.CSVLogger,
-                    flush_logs_every_n_steps=1,
-                    logger_kwargs={},
-                    dt=0.1,
-                    alpha=0.5,
-                    time_scale=None,
-                    loss_function=None,
-                   ):
+    def model_setup(
+        self,
+        dataset,
+        func,
+        log_path="./",
+        learning_rates=[1e-3],
+        optimizers=[torch.optim.Adam],
+        schedulers=[torch.optim.lr_scheduler.StepLR],
+        scheduler_kwargs=[{"step_size": 100, "gamma": 0.9}],
+        logger=pytorch_lightning.loggers.CSVLogger,
+        flush_logs_every_n_steps=1,
+        logger_kwargs={},
+        dt=0.1,
+        alpha=0.5,
+        time_scale=None,
+        loss_function=None,
+    ):
 
         """
         this is where we define all the options such as what forward function we should use, etc.
@@ -50,13 +50,14 @@ class BaseModel(pytorch_lightning.LightningModule):
         self._time_scale = time_scale
         self._alpha = alpha
         self._dt = dt
-        self.forward_function = base.ForwardFunctions(self.func,
-                                                      self._time_scale,
-                                                      self._alpha,
-                                                      self._dt,
-                                                      self.device,
-                                                     )
-        
+        self.forward_function = base.ForwardFunctions(
+            self.func,
+            self._time_scale,
+            self._alpha,
+            self._dt,
+            self.device,
+        )
+
         self._log_path = log_path
         self._flush_logs_every_n_steps = flush_logs_every_n_steps
         self._logger = logger(
@@ -64,7 +65,7 @@ class BaseModel(pytorch_lightning.LightningModule):
             flush_logs_every_n_steps=self._flush_logs_every_n_steps,
             **logger_kwargs,
         )
-        
+
         if not loss_function:
             self.loss_function = base.SinkhornDivergence()
         else:
@@ -76,43 +77,78 @@ class BaseModel(pytorch_lightning.LightningModule):
         X_hat = self.forward_function.step(self.func, X0=X0, t=t)
         W_hat = torch.ones_like(W_obs, requires_grad=True).to(self.device)
         sinkhorn_loss = self.loss_function.compute(W_hat, X_hat, W_obs, X_obs, t)
-        
-        return {"X_hat":X_hat, "sinkhorn_loss":sinkhorn_loss, "t": t}
+
+        return {"X_hat": X_hat, "sinkhorn_loss": sinkhorn_loss, "t": t}
 
     def training_step(self, batch, batch_idx):
 
         FWD = self.forward(X=batch, t=self.dataset._train_time)
-        base.update_loss_logs(self, FWD['sinkhorn_loss'], FWD['t'], self.current_epoch, batch_idx, "train", "sinkhorn", "d")
+        base.update_loss_logs(
+            self,
+            FWD["sinkhorn_loss"],
+            FWD["t"],
+            self.current_epoch,
+            batch_idx,
+            "train",
+            "sinkhorn",
+            "d",
+        )
 
-        return FWD['sinkhorn_loss'].sum()
-        
+        return FWD["sinkhorn_loss"].sum()
+
     def validation_step(self, batch, batch_idx):
-        
+
         base.retain_gradients(self)
         FWD = self.forward(X=batch, t=self.dataset._train_time)
-        base.update_loss_logs(self, FWD['sinkhorn_loss'], FWD['t'], self.current_epoch, batch_idx, "val", "sinkhorn", "d")
-        
-        return FWD['sinkhorn_loss']
-        
+        base.update_loss_logs(
+            self,
+            FWD["sinkhorn_loss"],
+            FWD["t"],
+            self.current_epoch,
+            batch_idx,
+            "val",
+            "sinkhorn",
+            "d",
+        )
+
+        return FWD["sinkhorn_loss"]
+
     def test_step(self, batch, batch_idx):
 
         base.retain_gradients(self)
         FWD = self.forward(X=batch, t=self.dataset._test_time)
-        base.update_loss_logs(self, FWD['sinkhorn_loss'], FWD['t'], self.current_epoch, batch_idx, "test", "sinkhorn", "d")
-        
-        return FWD['sinkhorn_loss']
-        
-    def configure_optimizers(self):    
-        return base.optim_config(param_groups=[self.parameters()],
-                                 learning_rates=self._learning_rates,
-                                 optimizers=self._optimizers,
-                                 schedulers=self._schedulers,
-                                 scheduler_kwargs=self._scheduler_kwargs,
-                                )
-    
+        base.update_loss_logs(
+            self,
+            FWD["sinkhorn_loss"],
+            FWD["t"],
+            self.current_epoch,
+            batch_idx,
+            "test",
+            "sinkhorn",
+            "d",
+        )
+
+        return FWD["sinkhorn_loss"]
+
+    def configure_optimizers(self):
+        return base.optim_config(
+            param_groups=[self.parameters()],
+            learning_rates=self._learning_rates,
+            optimizers=self._optimizers,
+            schedulers=self._schedulers,
+            scheduler_kwargs=self._scheduler_kwargs,
+        )
+
     def predict_step(self, batch, batch_idx, dataloader_idx=0):
-        FWD['sinkhorn_loss'] = self.forward(X=batch, t=self.dataset._test_time)
-        base.update_loss_logs(self, FWD['sinkhorn_loss'], FWD['t'], self.current_epoch, batch_idx, "predict", "sinkhorn", "d")
+        FWD["sinkhorn_loss"] = self.forward(X=batch, t=self.dataset._test_time)
+        base.update_loss_logs(
+            self,
+            FWD["sinkhorn_loss"],
+            FWD["t"],
+            self.current_epoch,
+            batch_idx,
+            "predict",
+            "sinkhorn",
+            "d",
+        )
         return FWD
-        
-        
