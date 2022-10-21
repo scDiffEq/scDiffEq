7c5fc094e8ce55a796469ae3e8ac671a65268c78
mvinyard <mvinyard@broadinstitute.org>
2022-10-21 15:17:11 +0000

    resolve conflict

--- scdiffeq/_models/__init__.py
+++ scdiffeq/_models/__init__.py
@@ -20,10 +20,7 @@ from ._scdiffeq import scDiffEq
 from ._base._core._base_model import BaseModel
 from . import _base as base
 
-<<<<<<< HEAD
 from ._base._core._prepare_data import prepare_data
 
-=======
->>>>>>> 2b690a534e502cdc43b2e976220475b8f72fdf16
-# to-do: ---------------------------------------------------------------------------------
+# TODO: ----------------------------------------------------------------------------------
 # from ._prescient import PRESCIENT
