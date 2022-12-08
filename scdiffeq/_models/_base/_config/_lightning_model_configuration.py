b1c34706827f1c647b0e180949c1a95ecf9c5147
mvinyard <mvinyard.ai@gmail.com>
2022-12-08 03:00:01 +0000

    adding restructured modules

---
+++ scdiffeq/_models/_base/_config/_lightning_model_configuration.py
@@ -0,0 +1,11 @@
+
+
+# from .._core._lightning_model import LightningModel
+
+# sdq.core.LightningModel(
+#     func,
+#     dt=0.1,
+#     optimizer_kwargs={},
+#     scheduler_kwargs={},
+#     **kwargs,
+# )
