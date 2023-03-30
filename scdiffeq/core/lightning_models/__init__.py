be4a4f04ae7feb955190072017644cf9a3673870
mvinyard <mvinyard.ai@gmail.com>
2023-03-30 20:19:54 +0000

    Add pre-train mixin

--- scdiffeq/core/lightning_models/__init__.py
+++ scdiffeq/core/lightning_models/__init__.py
@@ -5,6 +5,9 @@ from ._sinkhorn_divergence import SinkhornDivergence
 
 from . import base_models, mix_ins
 
-from ._lightning_ode import LightningODE, LightningPotentialODE
-from ._lightning_sde import LightningSDE, LightningPotentialSDE
+from ._lightning_ode import LightningODE
+from ._lightning_sde import LightningSDE
 from ._lightning_drift_net import LightningDriftNet, LightningPotentialDriftNet
+
+# LightningPotentialODE
+# LightningPotentialSDE
--- scdiffeq/core/lightning_models/mix_ins/__init__.py
+++ scdiffeq/core/lightning_models/mix_ins/__init__.py
@@ -1,2 +1,4 @@
 
 from ._potential_mix_in import PotentialMixIn
+from ._base_mix_in import BaseMixIn
+from ._pre_train_mix_in import PreTrainMixIn
