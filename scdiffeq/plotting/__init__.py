059fced301b8e45b80815b8c7386c265e122de14
mvinyard <mvinyard.ai@gmail.com>
2023-07-21 20:07:11 +0000

    Add plotting for smoothed expr

--- scdiffeq/plotting/__init__.py
+++ scdiffeq/plotting/__init__.py
@@ -6,3 +6,4 @@ __doc__ = """plotting __init__ module. Sub-package of the main scdiffeq API."""
 # import functions accessed as sdq.pl.<func>: --------------------------------------------
 from .potential_landscape import potential_landscape
 from ._velocity_stream import velocity_stream
+from ._smoothed_gene_expression import plot_smoothed_expression as smoothed_expression
