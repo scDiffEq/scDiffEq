69ceff870b219199327fdc2c4aa5e33b78a05641
mvinyard <mvinyard@broadinstitute.org>
2023-07-26 17:20:37 -0600

    various improvements to stabilize ABCParse

--- scdiffeq/core/utils/_abc_parse.py
+++ scdiffeq/core/utils/_abc_parse.py
@@ -1,6 +1,6 @@
+from typing import Dict, List, Any, Tuple
 from abc import ABC
-from typing import Dict, List, Any
-NoneType = type(None)
+
 
 class ABCParse(ABC):
     _BUILT = False
@@ -13,7 +13,6 @@ class ABCParse(ABC):
         pass
 
     def __build__(self) -> None:
-
         self._PARAMS = {}
         self._IGNORE = ["self", "__class__"]
         self._stored_private = []
@@ -24,8 +23,10 @@ class ABCParse(ABC):
     def __set__(
         self, key: str, val: Any, public: List = [], private: List = []
     ) -> None:
-
         self._PARAMS[key] = val
+        print(f"key: {key}")
+        print(f"private: {(key in private)}")
+        print(f"public: {(key in public)}\n")
         if (key in private) and (not key in public):
             self._stored_private.append(key)
             key = f"_{key}"
@@ -34,21 +35,35 @@ class ABCParse(ABC):
         setattr(self, key, val)
 
     def __set_existing__(self, key: str, val: Any) -> None:
-
-        self._PARAMS[key] = val
+        print(f"ALREADY FOUND: {key}")
+        passed_key = key
 
         if key in self._stored_private:
             key = f"_{key}"
-        setattr(self, key, val)
+
+        if passed_key == "kwargs":
+            attr = getattr(self, key)
+            print(key, attr)
+            attr.update(val)
+            print(key, attr)
+            setattr(self, key, attr)
+            self._PARAMS.update(val)
+        elif passed_key == "args":
+            attr = getattr(self, key)
+            print(key, attr)
+            attr += val
+            print(key, attr)
+            setattr(self, key, attr)
+            self._PARAMS[passed_key] += val
+        else:
+            self._PARAMS[passed_key] = val
+            setattr(self, key, val)
 
     @property
     def _STORED(self) -> List:
         return self._stored_private + self._stored_public
 
-    def __parse__(
-        self, kwargs: Dict, public: List = [], private: List = [], ignore: List = []
-    ):
-
+    def __setup_inputs__(self, kwargs, public, private, ignore) -> Tuple[List]:
         if not self._BUILT:
             self.__build__()
 
@@ -57,17 +72,29 @@ class ABCParse(ABC):
         if len(public) > 0:
             private = list(kwargs.keys())
 
+        return public, private
+
+    def __parse__(
+        self, kwargs: Dict, public: List = [], private: List = [], ignore: List = []
+    ) -> None:
+        """Central function of this class"""
+
+        public, private = self.__setup_inputs__(kwargs, public, private, ignore)
+
         for key, val in kwargs.items():
             if not key in self._IGNORE:
                 self.__set__(key, val, public, private)
 
-    def __update__(self, kwargs: dict, public: List = [], private: List = []) -> None:
+    def __update__(
+        self, kwargs: Dict, public: List = [], private: List = [], ignore: List = []
+    ) -> None:
+        """To be called after __parse__ has already been called."""
 
-        if not self._BUILT:
-            self.__build__()
+        public, private = self.__setup_inputs__(kwargs, public, private, ignore)
 
         for key, val in kwargs.items():
-            if not isinstance(val, NoneType) and key in self._STORED:
+            if not (val is None) and (key in self._STORED):
                 self.__set_existing__(key, val)
-            elif not isinstance(val, NoneType) and not key in self._IGNORE:
+
+            elif not (val is None) and not (key in self._IGNORE):
                 self.__set__(key, val, public, private)
