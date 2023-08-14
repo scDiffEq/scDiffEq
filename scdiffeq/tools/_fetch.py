e08dc0fb5dbfb087091da8feb970324951a532d5
mvinyard <mvinyard.ai@gmail.com>
2023-08-14 18:56:19 +0000

    rm fetch/formatting funcs

--- scdiffeq/tools/_fetch.py
+++
@@ -1,7 +0,0 @@
-
-# -- import packages: -------------
-from autodevice import AutoDevice
-import torch_adata
-
-def fetch(adata, use_key: str, device=AutoDevice()):
-    return torch_adata.tl.fetch(adata, use_key).to(device)
