6334c1a558140606a0c29fc61f3a09bd49bd7880
mvinyard <mvinyard.ai@gmail.com>
2023-10-21 17:23:36 +0000

    setting up tests/cov

--- setup.py
+++ setup.py
@@ -3,6 +3,10 @@ import re
 import os
 import sys
 
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 setuptools.setup(
     name="scdiffeq",
     version="0.0.48rc0",
@@ -14,28 +18,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     description="scDiffEq: modelling single-cell dynamics using neural differential equations.",
     packages=setuptools.find_packages(),
-    install_requires=[
-        "anndata>=0.9.1",
-        "torch>=2.0.0",
-        "pykeops>=2.1",
-        "geomloss>=0.2.5",
-        "licorice_font>=0.0.3",
-        "torchsde>=0.2.5",
-        "scikit-learn>=1.0.2",
-        "umap-learn>=0.5.3",
-        "lightning>=2.0.1",
-        "neural-diffeqs==0.3.1rc0",
-        "torch-nets>=0.0.4",
-        "torch-adata>=0.0.23",
-        "autodevice>=0.0.2",
-        "brownian-diffuser>=0.0.2",
-        "vinplots>=0.0.75",
-        "annoyance==0.0.18",
-        "ABCParse==0.0.3",
-        "scdiffeq-plots==0.0.1rc2",
-        "plotly==5.15.0",
-        "scvelo==0.2.5",
-    ],
+    install_requires=requirements,
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
         "Programming Language :: Python :: 3.9",
---
+++ tests/test_io.py
@@ -0,0 +1,80 @@
+
+import pathlib
+import os
+import numpy as np
+import anndata
+
+from scdiffeq import io
+
+class TestAnnData:
+#    def __init__(self, *args, **kwargs):
+#        ...
+
+    @property
+    def X(self):
+        return np.array(
+            [
+                [0, 0, 0, 0, 4, 0, 3],
+                [0, 0, 0, 4, 0, 0, 2],
+                [0, 0, 4, 0, 0, 0, 1],
+                [0, 4, 0, 0, 0, 0, 5],
+                [4, 0, 0, 0, 0, 0, 8],
+                [4, 0, 0, 0, 0, 0, 4],
+            ]
+        )
+
+    @property
+    def t(self):
+        return np.array([0, 0, 1, 1, 2, 2])
+
+    @property
+    def cell_barcodes(self):
+        return [
+            "ABC123",
+            "ABC456",
+            "DEF123",
+            "DEF456",
+            "ABC789",
+            "DEF789",
+        ]
+
+    @property
+    def cluster(self):
+        return [
+            "A",
+            "B",
+            "C",
+            "A",
+            "B",
+            "C",
+        ]
+
+    @property
+    def adata(self):
+        adata = anndata.AnnData(X=self.X) # , dtype=self.X.dtype)
+        adata.obs["cell_barcode"] = self.cell_barcodes
+        adata.obs["t"] = self.t
+        adata.obs["cluster"] = self.cluster
+        return adata
+
+    def __call__(self, *args, **kwargs):
+
+        return self.adata
+
+
+def test_read_h5ad():
+
+    h5ad_path = pathlib.Path("pytest_adata.h5ad")
+
+    if not h5ad_path.exists():
+        test_anndata = TestAnnData()
+
+        adata = test_anndata()
+        adata.write_h5ad(h5ad_path)
+
+    adata = io.read_h5ad(h5ad_path)
+
+    assert adata is not None, "AnnData object is None"
+    assert hasattr(adata, "X"), "AnnData object doesn't have the main data matrix"
+
+    os.remove("pytest_adata.h5ad")
