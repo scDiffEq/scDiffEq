1806b931946fc4388fbb53627cc9b28da3c1f214
Michael Vinyard <47393421+mvinyard@users.noreply.github.com>
2023-11-07 07:58:34 -0700

    Update __init__.py

--- scdiffeq/core/callbacks/__init__.py
+++ scdiffeq/core/callbacks/__init__.py
@@ -5,6 +5,6 @@ from ._intermittent_saves import IntermittentSaves
 from ._gradient_potential_callback import GradientPotentialTest
 from ._testing import Testing
 
-from ._visualize_tracked_loss import ModelTracker, VisualizeTrackedLoss
+# from ._visualize_tracked_loss import ModelTracker, VisualizeTrackedLoss
 from ._visualize_predictions import VisualizePredictions
 from ._model_logging import ModelLogging
