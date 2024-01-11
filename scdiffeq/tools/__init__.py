94d7bc4a06d83fd1f67896bf18cc3725fa36938f
mvinyard <mvinyard@broadinstitute.org>
2024-01-11 09:47:24 -0700

    rm old func

--- scdiffeq/tools/__init__.py
+++ scdiffeq/tools/__init__.py
@@ -8,19 +8,19 @@ __email__ = ", ".join(["mvinyard.ai@gmail.org",])
 # import functions accessed as sdq.tl.<func>: --------------------------------------------
 from ._annotate_cells import annotate_cells
 from ._time_free_sampling import time_free_sampling
-from ._hyperparams import HyperParams
+
 from ._reconstruct_function import reconstruct_function
 from ._versions import Versions, configure_version
 from ._func_from_version import func_from_version
 from ._umap import UMAP
-# from ._fetch import fetch
+
 from ._drift_diffusion_state_characterization import drift, diffusion
 
 from ._dimension_reduction import DimensionReduction
 
 
 # -----------
-# from ._data_formatter import DataFormatter
+
 # from ._x_use import X_use, fetch_formatted_data
 from ._knn import kNN
 from ._knn_smoothing import kNNSmoothing
