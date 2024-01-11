e5c1f50552be6702e9f8f6210adceb79a90b0431
mvinyard <mvinyard@broadinstitute.org>
2024-01-11 10:53:33 -0700

    update pickle docs

--- scdiffeq/io/_data/_pickle_io.py
+++ scdiffeq/io/_data/_pickle_io.py
@@ -1,9 +1,13 @@
 
 import pickle
 
+import pathlib
+
+from typing import Any, Dict, Optional, Union
 
 class PickleIO:
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
+        """ """
         pass
 
     def read(self, path, mode="rb"):
@@ -18,13 +22,42 @@ class PickleIO:
         return open(path, mode)
     
     
-def read_pickle(path, mode="rb"):
+def read_pickle(path: Union[str, pathlib.Path], mode: Optional[str] = "rb"):
+    """Read the contents of a pickle file into memory.
+    
+    Args:        
+        path (Union[str, pathlib.Path]): pickle file path. 
+        
+        mode (Optional[str]): read mode. **Default** = "rb"
+                
+    Returns:
+        obj (Dict[Any,Any]): Object, usually a dictionary contained in pickle file.
+    """
 
     pickle_io = PickleIO()
     return pickle_io.read(path, mode=mode)
 
 
-def write_pickle(obj, path, mode="wb", protocol=pickle.HIGHEST_PROTOCOL):
+def write_pickle(
+    obj: Dict[Any,Any],
+    path: Union[str, pathlib.Path],
+    mode: Optional[str] = "wb",
+    protocol: Optional = pickle.HIGHEST_PROTOCOL,
+):
+    """Save an object to a pickle file.
+    
+    Args:
+        obj (Dict[Any,Any]): Object, usually a dictionary to write to pickle file.
+        
+        path (Union[str, pathlib.Path]): path to which pickle file should be written. 
+        
+        mode (Optional[str]): write mode. **Default** = "wb"
+        
+        protocol (Optional) pickling protocol. **Default** = ``pickle.HIGHEST_PROTOCOL``
+        
+    Returns:
+        None
+    """
 
     pickle_io = PickleIO()
-    return pickle_io.write(obj=obj, path=path, mode=mode, protocol=protocol)
+    pickle_io.write(obj=obj, path=path, mode=mode, protocol=protocol)
