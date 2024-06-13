499562675fbfa0653a49d1c63180eaaa9c1a6d3f
mvinyard <mvinyard.ai@gmail.com>
2024-06-13 17:15:21 +0000

    org

--- scdiffeq/__init__.py
+++ scdiffeq/__init__.py
@@ -5,26 +5,28 @@ __author__ = ", ".join(["Michael E. Vinyard"])
 __email__ = ", ".join(["mvinyard.ai@gmail.com"])
 
 
-# -- setup logging: ------
+# -- setup logging: -----------------------------------------------------------
 import py_pkg_logging as _ppl
 _LOGGING = _ppl.PackageLogging(name = __name__, file = __file__)
 
 
-from .__version__ import __version__
-
+# -- handle dependency-related warnings: --------------------------------------
 import os as _os
 _os.environ["KEOPS_VERBOSE"] = "0"
 
 
-# -- import model API: -------------------------------------------------------------------
+# -- import model API: --------------------------------------------------------
 from .core._scdiffeq import scDiffEq
 
 
-# -- import sub-packages: ----------------------------------------------------------------
+# -- import sub-packages: -----------------------------------------------------
 from . import core
 from . import io
 from . import plotting as pl
 from . import tools as tl
 from . import datasets
-
 from . import _backend_utilities as utils
+
+
+# -- version: -----------------------------------------------------------------
+from .__version__ import __version__
