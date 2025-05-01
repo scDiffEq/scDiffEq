8ee3e81158f7171bb6076ef107ca583100a7860a
mvinyard <mvinyard.ai@gmail.com>
2025-05-01 16:52:24 +0000

    cleanup

--- scdiffeq/_backend_utilities/__init__.py
+++ scdiffeq/_backend_utilities/__init__.py
@@ -1,3 +1,2 @@
 
 from ._anndata_index_lookup import ObsIndexLookUp, VarIndexLookUp
-from ._log_memory_usage import log_memory_usage
--- scdiffeq/_backend_utilities/_log_memory_usage.py
+++
@@ -1,6 +0,0 @@
-import psutil, os
-
-def log_memory_usage(epoch):
-    process = psutil.Process(os.getpid())
-    mem = process.memory_info().rss / 1024 ** 2  # in MB
-    print(f"Epoch {epoch} - Memory Usage: {mem:.2f} MB")
