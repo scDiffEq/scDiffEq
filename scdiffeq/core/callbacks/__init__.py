b51c1b9ed68aebfe4d29215e82b909516a0c358d
mvinyard <mvinyard@broadinstitute.org>
2025-05-03 10:36:20 -0600

    Cleanup

--- scdiffeq/core/callbacks/__init__.py
+++ scdiffeq/core/callbacks/__init__.py
@@ -4,3 +4,12 @@ from ._testing import Testing
 from ._model_logging import ModelLogging
 from ._memory_monitor import MemoryMonitor
 from ._basic_progress_bar import BasicProgressBar
+
+__all__ = [
+    "IntermittentSaves",
+    "GradientPotentialTest",
+    "Testing",
+    "ModelLogging",
+    "MemoryMonitor",
+    "BasicProgressBar",
+]
--- scdiffeq/core/callbacks/_intermittent_saves.py
+++ scdiffeq/core/callbacks/_intermittent_saves.py
@@ -1,27 +1,50 @@
-
-from lightning import Callback
-import pickle
+# -- import packages: ---------------------------------------------------------
+import lightning
+import logging
 import os
+import pickle
+
+# -- configure logger: --------------------------------------------------------
+logger = logging.getLogger(__name__)
 
 
-class IntermittentSaves(Callback):
-    def __init__(self, frequency=50):
-                
+# -- callback cls: ------------------------------------------------------------
+class IntermittentSaves(lightning.pytorch.callbacks.Callback):
+    def __init__(self, frequency: int = 50) -> None:
+        """
+        Args:
+            frequency (int): save frequency
+        """
+
+        super().__init__()
+
         self.frequency = frequency
 
-    def on_train_batch_end(self, trainer, model, outputs, batch, batch_idx, *args, **kwargs):
-        
-        """ """
-                
+    def on_train_batch_end(
+        self, trainer, model, outputs, batch, batch_idx, *args, **kwargs
+    ):
+        """
+        Args:
+            trainer (lightning.pytorch.Trainer): trainer
+            model (lightning.pytorch.LightningModule): model
+            outputs (dict): outputs
+            batch (torch.Tensor): batch
+            batch_idx (int): batch index
+        """
+
         epoch = model.current_epoch
-        
+
         if (epoch % self.frequency == 0) and (batch_idx == 0):
             dpath = os.path.join(trainer.logger.log_dir, "ckpt_outputs")
             if not os.path.exists(dpath):
                 os.mkdir(dpath)
+                logger.info(f"Created directory: {dpath}")
 
-            fpath = os.path.join(dpath, "epoch_{}.batch_{}.pkl".format(epoch, batch_idx))
-            pickle.dump(obj=outputs,
-                        file=open(fpath, "wb"),
-                        protocol=pickle.HIGHEST_PROTOCOL,
-                       )
+            fpath = os.path.join(
+                dpath, "epoch_{}.batch_{}.pkl".format(epoch, batch_idx)
+            )
+            pickle.dump(
+                obj=outputs,
+                file=open(fpath, "wb"),
+                protocol=pickle.HIGHEST_PROTOCOL,
+            )
