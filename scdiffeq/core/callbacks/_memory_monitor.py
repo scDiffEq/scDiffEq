56ef06e556169670d845c0d8e66e4806b861c363
Michael Vinyard <mvinyard.ai@gmail.com>
2025-05-18 13:26:10 -0600

    cleanup annoying print statements

--- scdiffeq/core/callbacks/_memory_monitor.py
+++ scdiffeq/core/callbacks/_memory_monitor.py
@@ -1,9 +1,12 @@
 # -- import packages: ----------------------------------------------------------
 import gc
 import lightning
+import logging
 import os
 import torch
 
+# -- configure logger: ---------------------------------------------------------
+logger = logging.getLogger(__name__)
 
 # -- operational cls: ----------------------------------------------------------
 class MemoryMonitor(lightning.pytorch.callbacks.Callback):
@@ -22,7 +25,7 @@ class MemoryMonitor(lightning.pytorch.callbacks.Callback):
         import wandb
 
         mem = self.process.memory_info().rss / 1024**2  # in MB
-        print(f"[MemoryLogger] Epoch {trainer.current_epoch}: CPU RAM: {mem:.2f} MB")
+        logger.debug(f"[MemoryLogger] Epoch {trainer.current_epoch}: CPU RAM: {mem:.2f} MB")
         wandb.log({"Memory/CPU_RAM_MB": mem})
 
     def _log_gpu_ram(self, trainer, lit_module):
@@ -33,9 +36,7 @@ class MemoryMonitor(lightning.pytorch.callbacks.Callback):
             gpu_mem_allocated = lit_module.device
             mem_allocated = torch.cuda.memory_allocated(lit_module.device) / 1024**2
             mem_reserved = torch.cuda.memory_reserved(lit_module.device) / 1024**2
-            print(
-                f"[MemoryLogger] Epoch {trainer.current_epoch}: GPU Allocated: {mem_allocated:.2f} MB | Reserved: {mem_reserved:.2f} MB"
-            )
+            logger.debug(f"[MemoryLogger] Epoch {trainer.current_epoch}: GPU Allocated: {mem_allocated:.2f} MB | Reserved: {mem_reserved:.2f} MB")
             wandb.log(
                 {
                     "Memory/GPU_Allocated_MB": mem_allocated,
@@ -45,10 +46,8 @@ class MemoryMonitor(lightning.pytorch.callbacks.Callback):
 
     def _report_object_memories(self, trainer):
         objs = gc.get_objects()
-        print(
-            f"[MemoryLogger] Epoch {trainer.current_epoch}: Number of tracked objects: {len(objs)}"
-        )
-
+        logger.debug(f"[MemoryLogger] Epoch {trainer.current_epoch}: Number of tracked objects: {len(objs)}")
+        
     def on_train_epoch_end(self, trainer, lit_module):
 
         self._log_cpu_ram(trainer=trainer)
