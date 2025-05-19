53bcb20a5e5299b4447a995209cd16fa5addc5e2
Michael Vinyard <mvinyard.ai@gmail.com>
2025-05-18 22:30:32 -0600

    add gw

--- scdiffeq/datasets/__init__.py
+++ scdiffeq/datasets/__init__.py
@@ -1,5 +1,11 @@
 from ._human_hematopoiesis import human_hematopoiesis
 from ._pancreatic_endocrinogenesis import pancreatic_endocrinogenesis
 from ._larry_in_vitro import larry
+from ._larry_kegg_growth_weights import larry_kegg_growth_weights
 
-__all__ = ["pancreatic_endocrinogenesis", "larry", "human_hematopoiesis"]
+__all__ = [
+    "pancreatic_endocrinogenesis",
+    "larry",
+    "human_hematopoiesis",
+    "larry_kegg_growth_weights",
+]
